277ab48711867a928ecfbc9df8668f9b1f15e3fa
ADAM DARWISH AZMAN <adam_22011781@utp.edu.my>
2026-02-26 07:24:46 +0800

    Add lab 4 tasks.

---
+++ 2026_02_09-Lab4/Task3.R
@@ -0,0 +1,9 @@
+array1 = array(1:24, dim = c(2, 4, 3))
+array2 = array(25:54, dim = c(3, 2, 5))
+
+
+print("The second row of second matrix of first array:")
+print(array1[2, , 2])
+
+print("The element at second column and third row of second matrix of second array:\n")
+print(array2[3, 2, 1])
